097e83c1b53bbe937a0d345068ca5f1ce9bdbb02
Nduka Ugochukwu <62814725+thatzerroguy@users.noreply.github.com>
2025-10-20 08:41:16 +0100

    feat: complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export", // 👈 This enables static export
+    reactStrictMode: true,
 };
 
 export default nextConfig;
